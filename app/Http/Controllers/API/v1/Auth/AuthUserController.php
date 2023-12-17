1f180e7f8b6e22774d108e08821b0abb6685de4e
FatefulNur <km.nurunnabi66@gmail.com>
2023-12-17 18:32:36 +0600

    Added authenticate feature

---
+++ app/Http/Controllers/API/v1/Auth/AuthUserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthUserRequest;
+use Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthUserController extends Controller
+{
+    public function create(AuthUserRequest $request)
+    {
+        if (!Auth::attempt($request->validated())) {
+            return response()->json([
+                'message' => 'Unauthenticated (User Not Exist).',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = $request->user()
+            ->createToken($request->input('email'))
+            ->plainTextToken;
+
+        return response()->json([
+            'token_type' => 'Bearer',
+            'token' => $token,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\v1\UrlController;
+use App\Http\Controllers\API\v1\Auth\AuthUserController;
+use App\Http\Controllers\API\v1\Auth\RegisterUserController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [RegisterUserController::class, 'store'])->name('register');
+    Route::post('/tokens/create', [AuthUserController::class, 'create'])->name('tokens.create');
 });
