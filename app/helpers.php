0c6577a606075ce3a6360242da8cecbfe2954296
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-31 12:35:58 +0600

    Updated generateShortUrl helper logic

--- app/helpers.php
+++ app/helpers.php
@@ -2,17 +2,12 @@
 
 use App\Models\Url;
 
-if (! function_exists('generateShortUrl')) {
+if (!function_exists('generateShortUrl')) {
     function generateShortUrl()
     {
         $generatedUrl = url(str()->random(8));
 
-        $existedUrl = Url::firstWhere('short_url', $generatedUrl);
-
-        if ($existedUrl) {
-            return generateShortUrl();
-        }
-
-        return $generatedUrl;
+        return Url::where('short_url', $generatedUrl)
+            ->firstOr(fn() => $generatedUrl);
     }
 }
